18048c1e2ce0e50147d9978188b688d985f18376
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-11-22 13:09:31 +0100

    Add files via upload

---
+++ src/Pages/helpers/AboutText.jsx
@@ -0,0 +1,6 @@
+export default function AboutText() {
+  return (
+  <><p>Ich bin Julia Breitenbruch,  IT-begeistert mit einem starken Interesse an der Lösung komplexer Probleme und der Umsetzung kreativer Ideen.</p> <p>Ursprünglich als SAS-Programmiererin in diversen CROs tätig, habe ich in den letzten Jahren viel Zeit und Energie investiert, um meine Kenntnisse in verschiedenen IT-Bereichen, einschließlich Frontend-Entwicklung und Data Science, kontinuierlich zu erweitern.</p>
+<p>
+Wenn Sie jemanden suchen, der nicht nur technische Fähigkeiten, sondern auch Begeisterung und Engagement  mitbringt, dann bin ich bereit, meinen Beitrag zu leisten und gemeinsam mit Ihrem Team großartige Projekte umzusetzen.</p></>
+)}
---
+++ src/Pages/helpers/certs.jsx
@@ -0,0 +1,10 @@
+export const certs=[{id:1,url:"https://www.freecodecamp.org/certification/fcc2df8121a-96df-42ad-a505-8b5f54977098/javascript-algorithms-and-data-structures",desc:"Javascript Algorithms and Data Structures"},
+{id:2,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/relational-database-v8",desc:"Relational Database"},
+{id:3,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/responsive-web-design",desc:"Responsive Webdesign"},
+{id:4,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/front-end-development-libraries",desc:"Front End Development Libraries"},
+{id:5,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/data-visualization",desc:"Data Visualization"},
+{id:6,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/data-analysis-with-python-v7",desc:"Data Analysis with Python"},
+{id:7,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/scientific-computing-with-python-v7",desc:"Scientific Computing with Python"},
+{id:8,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/machine-learning-with-python-v7",desc:"Machine Learning with Python"},
+{id:9,url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/back-end-development-and-apis",desc:"Back End Development and APIs"},
+{id:10, url:"https://www.freecodecamp.org/certification/fccbcd44a63-082a-4123-badf-9baf7375d0c6/quality-assurance-v7",desc:"Quality Assurance"}];
