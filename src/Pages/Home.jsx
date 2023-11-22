c684b28cff809b3ca95480d47ab20bd999b1e755
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-11-22 13:17:06 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 import "../styles/Home.css";
 import wordcloud from './assets/wordcloud.png';
-import {Footer} from './Footer';
+
 import Type from './Type';
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material
 export const Home = () => {
   return (
    <><div className="home">
@@ -10,6 +13,8 @@ export const Home = () => {
                 <Type />
               </div>
     <img src={wordcloud} id="wordcloud"/>
-    </div><Footer/></>
+      <a href="mailto:julia.breitenbruch@gmail.com"><EmailIcon /></a>
+      <a href="https://www.github.com/JBreitenbr"><GithubIcon /></a>
+      <a href="tel:+4915253917250"><PhoneIcon /></a></div></>
   );
 };
