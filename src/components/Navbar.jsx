dc60e4dc7073c1c8804d51660125ffa66f43d961
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-11-22 13:22:36 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import {Link} from 'react-router-dom';
 import Giulietta from './assets/Giulietta.png';
-export const Navbar=() => {
+{/*export const Navbar=() => {
   return ( 
     <nav id="navbar" className="navbar navbar-expand-sm navbar-light" style={{backgroundColor:"#f3f3f3"}}><Link to="/" className="navbar-brand" >
     <img className="jull-brand" src={Giulietta}/></Link><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,4 +16,39 @@ export const Navbar=() => {
 >Zertifizierungen</Link>
 </div> 
  </nav>);
- }
+ }*/}
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/projects"> Projects </Link>
+        <Link to="/certifications"> Certifications </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
