bd9701ed2cd89329eed1f0ff766e762e2078792c
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-11-22 13:41:37 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,7 @@
-import Giulietta from './assets/Giulietta.png';
-{/*export const Navbar=() => {
-  return ( 
-    <nav id="navbar" className="navbar navbar-expand-sm navbar-light" style={{backgroundColor:"#f3f3f3"}}><Link to="/" className="navbar-brand" >
-    <img className="jull-brand" src={Giulietta}/></Link><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-<div className="collapse navbar-collapse" id="navbarNav">
-<Link to="/" className="nav-link nav-item nav-custom" 
->Start</Link>
-<Link to="/about" className="nav-link nav-item nav-custom" 
->Über mich</Link>
-<Link to="/projects" className="nav-link nav-item nav-custom" 
->Projekte</Link>
- <Link to="/certifications" className="nav-link nav-item nav-custom" 
->Zertifizierungen</Link>
-</div> 
- </nav>);
- }*/}
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link} from "react-router-dom";
 import "../styles/Navbar.css";
-/*import ReorderIcon from "@material-ui/icons/Reorder";*/
+
 
 export const Navbar=() => {
   const [expandNavbar, setExpandNavbar] = useState(false);
