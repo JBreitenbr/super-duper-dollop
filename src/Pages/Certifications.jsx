8945a1b7a5d88d3c595644e7bd7d2d4ce94a5d41
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2023-11-22 14:20:35 +0100

    Update Certifications.jsx

--- src/Pages/Certifications.jsx
+++ src/Pages/Certifications.jsx
@@ -1,5 +1,4 @@
 import "../styles/Certifications.css";
-import {Footer} from './Footer';
 import {certs} from "./helpers/certs";
 const listItems =certs.map(
 (item) => {
@@ -12,12 +11,11 @@ return (
 
 export const Certifications = () => {
   return (
-   <> <div className="certs">
+   <div className="certs">
       <div className="certs-container">
       <div className="certs-wrapper">  <p className="pre-list">Im folgenden eine Liste meiner Zertifizierungen auf freeCodeCamp (es mussten dafür jeweils fünf Projekte erstellt werden, welche im unteren Bereich des jeweiligen Dokuments verlinkt sind): </p>
   <div>{listItems}</div>
       </div></div>
   </div>
-     <Footer/></>
   );
 };
